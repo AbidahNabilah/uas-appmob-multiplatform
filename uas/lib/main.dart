915dc2a786d22a83bfe6d0e9fbdb9549f596b3fc
AbidahNabilah <abdhnabilah@gmail.com>
2021-06-28 12:09:47 +0700

    Mulai Ujian

--- uas/lib/main.dart
+++ uas/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'UAS Pengembangan AppMobile Multiplaform',
+      title: 'UAS - Appmob Multiplatform',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
